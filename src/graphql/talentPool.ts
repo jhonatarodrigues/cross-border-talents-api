b731b36847cbda67cf6a3f4c2481ae61d5cc705a
jhonatarodrigues <jhonata.a.r@hotmail.com>
2022-06-29 16:44:47 -0300

    code review

--- src/graphql/talentPool.ts
+++ src/graphql/talentPool.ts
@@ -23,7 +23,7 @@ export const TypeDefs: ITypedef = `
 `;
 
 export const Query = `
-  talentPools(search: String, limit: String, country: String, department: String, language: String, teamLeader: string): [UserTalentPool]
+  talentPools(search: String, limit: String, country: String, department: String, language: String, teamLeader: String): [UserTalentPool]
   talentPool(idUser: ID!): UserTalentPool
 `;
 
